6fb69ea78d63102edb79df01bb66979f28ed9912
VilizarTodorov <todorovvilizar@gmail.com>
2021-05-12 15:15:09 +0300

    reworked sidebar

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,16 +1,14 @@
-import { Avatar, Button, IconButton } from "@material-ui/core";
-import ChatIcon from "@material-ui/icons/Chat";
+import { Button } from "@material-ui/core";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
 import styles from "../styles/Sidebar.module.css";
 import { useUser } from "../UserContext";
 import Contacts from "./Contacts";
-import ListOfPeople from "./List";
-import Person from "./Person";
 import Profile from "./Profile";
 import Search from "./Search";
+import Chats from "./SidebarComponents/Chats";
 import SidebarHeader from "./SidebarComponents/SidebarHeader";
-import SidebarMoreVertIconMenu from "./SidebarMoreVertIconMenu";
+import SidebarContainer from "./SidebarComponents/SidebarContainer";
 
 const Sidebar = (props) => {
   const [isProfileOpen, setIsProfileOpen] = useState(false);
@@ -26,31 +24,17 @@ const Sidebar = (props) => {
   };
 
   return (
-    <div className={styles.container}>
+    <SidebarContainer>
       <SidebarHeader
         openProfile={() => setIsProfileOpen(true)}
         openContacts={() => setIsContactsOpen(true)}
       ></SidebarHeader>
       <Search></Search>
       <Button className={styles.newChatButton}>START NEW CHAT</Button>
-      <ListOfPeople>
-        {user.chats.map((chat) => {
-          const email = chat.users.filter((x) => x !== user.userDbEntry.email)[0];
-          const contact = user.contacts.find((contact) => contact.email === email);
-          return (
-            <Person
-              key={`${email}chat`}
-              callbackFunction={callbackFunction(chat.id)}
-              email={email}
-              photoUrl={contact?.photo}
-              message={"Hi I'm using chat"}
-            ></Person>
-          );
-        })}
-      </ListOfPeople>
+      <Chats chats={user.chats} user={user} redirectFunction={callbackFunction}></Chats>
       <Profile close={() => setIsProfileOpen(false)} isOpen={isProfileOpen}></Profile>
       <Contacts close={() => setIsContactsOpen(false)} isOpen={isContactsOpen}></Contacts>
-    </div>
+    </SidebarContainer>
   );
 };
 
