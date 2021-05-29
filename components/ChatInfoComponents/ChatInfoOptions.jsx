61284edf6ad112d832dcf66a92671648308a1398
VilizarTodorov <todorovvilizar@gmail.com>
2021-05-29 14:56:20 +0300

    refactor

---
+++ components/ChatInfoComponents/ChatInfoOptions.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GeneralContainer from './GeneralContainer'
+
+const ChatInfoOptions = () => {
+    return (
+        <GeneralContainer>
+            
+        </GeneralContainer>
+    )
+}
+
+export default ChatInfoOptions
